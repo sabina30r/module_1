3eed42b7708c5bee519d6555add8ca82717b2cf6
sabina30r <sabina3005.15@gmail.com>
2020-06-19 21:41:37 +0300

    task 1 of level 2 nix_edu module done

--- module-app/src/main/java/nix/edu/App.java
+++ module-app/src/main/java/nix/edu/App.java
@@ -3,12 +3,21 @@ package nix.edu;
 public class App {
     public static void main(String[] args) {
         int[] arr = {1,4,5,1,1,3};
+        String stringForTask1 = "{module( of nix ) educational} [program]";
         Lev1Util lev1Util = new Lev1Util();
+        Lev2Util lev2Util = new Lev2Util();
+        System.out.println("Level 1");
         System.out.print("Number of unique digits: ");
         lev1Util.countArrayUniqueDigits(arr);
         System.out.print("Check possibility of horse move: ");
         lev1Util.checkHorseMove(1,1,2,2);
         System.out.print("Triangle's square: ");
         lev1Util.findTriangleSquare(1,2,5,1,5,3);
+        System.out.println();
+        System.out.println("Level 2");
+        System.out.print("Is input string correct? : ");
+        lev2Util.checkStringWithBrackets(stringForTask1);
+
+
     }
 }
---
+++ module-lev-2/src/main/java/nix/edu/Lev2Util.java
@@ -0,0 +1,38 @@
+package nix.edu;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class Lev2Util {
+    public void checkStringWithBrackets(String inputString) {
+
+        HashMap<String, Integer> openBrackets = new HashMap<String, Integer>() {{
+            put("(", 0);
+            put("{", 1);
+            put("[", 2);
+        }};
+        HashMap<String, Integer> closeBrackets = new HashMap<String, Integer>() {{
+            put(")", 0);
+            put("}", 1);
+            put("]", 2);
+        }};
+        Stack<String> stack = new Stack<>();
+
+        String result = "YES";
+
+        for (int i = 0; i < inputString.length(); i++) {
+            Character character = inputString.charAt(i);
+            String symbol = character.toString();
+            if (openBrackets.containsKey(symbol)) {
+                stack.push(symbol);
+            } else if (closeBrackets.containsKey(symbol)) {
+                if (stack.empty() || !openBrackets.get(stack.pop()).equals(closeBrackets.get(symbol))) {
+                    result = "NO";
+                }
+            }
+        }
+        System.out.println(result);
+    }
+    
+
+}
