6e93747291ce575925fa50890cbf8e45f7b053c5
sabina30r <sabina3005.15@gmail.com>
2020-06-19 20:34:21 +0300

    task 2 of level 1 nix_edu module done

--- module-app/src/main/java/nix/edu/App.java
+++ module-app/src/main/java/nix/edu/App.java
@@ -4,7 +4,9 @@ public class App {
     public static void main(String[] args) {
         int[] arr = {1,4,5,1,1,3};
         Lev1Util lev1Util = new Lev1Util();
-        System.out.println("Number of unique digits: ");
+        System.out.print("Number of unique digits: ");
         lev1Util.countArrayUniqueDigits(arr);
+        System.out.print("Check possibility of horse move: ");
+        lev1Util.checkHorseMove(1,1,2,2);
     }
 }
--- module-lev-1/src/main/java/nix/edu/Lev1Util.java
+++ module-lev-1/src/main/java/nix/edu/Lev1Util.java
@@ -11,9 +11,17 @@ public class Lev1Util {
                 .boxed()
                 .toArray(Integer[]::new);
         Set<Integer> resultSet = new HashSet<Integer>(Arrays.asList(inputArr));
-        System.out.print(resultSet.size());
+        System.out.println(resultSet.size());
         return resultSet.size();
 
+    }
+
+    public void checkHorseMove(int currentRow, int currentColumn, int supposedRow, int supposedColumn){
+       int rowDiff = Math.abs(currentRow - supposedRow);
+       int colDiff = Math.abs(currentColumn - supposedColumn);
+        if (rowDiff == 1 & colDiff == 2 | rowDiff == 2 & colDiff == 1 )
+            System.out.println("It's possible to make move");
+        else System.out.println("It's impossible to make move");
     }
 
 }
